37025d2540a20304e8bab04da2b6f6fb921439b4
AlaaMaHdii <s190434@dtu.dk>
2022-12-02 17:14:38 +0100

    Deaktiveret 2FA by default

--- src/main/java/com/web/ehrapp/TwilioApiKeys.java
+++ src/main/java/com/web/ehrapp/TwilioApiKeys.java
@@ -1,8 +1,8 @@
 package com.web.ehrapp;
 
 public class TwilioApiKeys{
-        public static final String SID = "FJERNET";
-        public static final String PASSWORD = "FJERNET";
-        public static final String VERIFY_SERVICE = "FJERNET";
-        public static final Boolean OTP_ENABLED = true;
+        public static final String SID = "Fjernet";
+        public static final String PASSWORD = "Fjernet";
+        public static final String VERIFY_SERVICE = "Fjernet";
+        public static final Boolean OTP_ENABLED = false;
 }
